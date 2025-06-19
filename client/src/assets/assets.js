dd5c881d2495a578ede5d33875791feb30cf9f16
RawanYasser297 <157253729+RawanYasser297@users.noreply.github.com>
2025-06-19 10:38:33 +0300

    Update assets.js

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,42 +1,38 @@
 
-const categoryList=[
-    {
-        'name':'Breakfast',
-        'img':'https://tomato-market-mern-app-server.onrender.com/images/Ovo Frito Desenho Para Colorir.jpeg'
-    },
-    {
-        'name':'Snacks',
-        'img':'https://tomato-market-mern-app-server.onrender.com/images/Grapes free icons designed by Smashicons (1).jpeg'
-    },
+const categoryList = [
+  {
+    name: 'Breakfast',
+    img: 'https://tomato-market-mern-app-server.onrender.com/images/Ovo%20Frito%20Desenho%20Para%20Colorir.jpeg',
+  },
+  {
+    name: 'Snacks',
+    img: 'https://tomato-market-mern-app-server.onrender.com/images/Grapes%20free%20icons%20designed%20by%20Smashicons%20(1).jpeg',
+  },
+  {
+    name: 'Hot drink',
+    img: 'https://tomato-market-mern-app-server.onrender.com/images/Tea%20Cup.jpeg',
+  },
+  {
+    name: 'Main Dishes',
+    img: 'https://tomato-market-mern-app-server.onrender.com/images/dinner.jpeg',
+  },
+  {
+    name: 'Salad',
+    img: 'https://tomato-market-mern-app-server.onrender.com/images/salad.jpeg',
+  },
+  {
+    name: 'Appetizer',
+    img: 'https://tomato-market-mern-app-server.onrender.com/images/French%20Fries%20.jpeg',
+  },
+  {
+    name: 'Dessert',
+    img: 'https://tomato-market-mern-app-server.onrender.com/images/30%20free%20icons%20of%20Ice%20cream%20designed%20by%20Handicon.jpeg',
+  },
+  {
+    name: 'Cold drink',
+    img: 'https://tomato-market-mern-app-server.onrender.com/images/lemon.jpeg',
+  },
+];
 
-{
-    'name':'Hot drink',
-    'img':'https://tomato-market-mern-app-server.onrender.com/images/Tea Cup.jpeg'
-},
+export default categoryList;
 
-{
-    'name':'Main Dishes',
-    'img':'https://tomato-market-mern-app-server.onrender.com/images/dinner.jpeg'
-},
-
-{
-    'name':'Salad',
-    'img':'https://tomato-market-mern-app-server.onrender.com/images/salad.jpeg'
-},
-{
-    'name':'Appetizer',
-    'img':'https://tomato-market-mern-app-server.onrender.com/images/French Fries .jpeg'
-},
-
-{
-    'name':'Dessert',
-    'img':'https://tomato-market-mern-app-server.onrender.com/images/30 free icons of Ice cream designed by Handicon.jpeg'
-},
-{
-    'name':'Cold drink',
-    'img':'https://tomato-market-mern-app-server.onrender.com/images/lemon.jpeg'
-},
-
-]
-
-export default categoryList
