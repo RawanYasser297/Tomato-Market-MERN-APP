7aff0c9f96ac5ab3c14e9f3cab213509d7950a40
RawanYasser297 <157253729+RawanYasser297@users.noreply.github.com>
2025-06-20 14:18:20 +0300

    Update EditItem.jsx

--- admin/src/components/EditItem.jsx
+++ admin/src/components/EditItem.jsx
@@ -1,7 +1,8 @@
-import {useParams,Link} from 'react-router-dom'
+import {useParams,Link,useNavigate} from 'react-router-dom'
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import categoryList from '../category';
+
 
 const EditItem = () => {
   
